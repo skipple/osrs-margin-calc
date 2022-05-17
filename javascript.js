4b6fe1c2a8d782f57e298875752429ca12434b79
skipple <kevin.read@live.com>
2022-05-17 10:32:43 -0400

    Allow entering of K, M, B values. Addition of ROI value.

--- javascript.js
+++ javascript.js
@@ -10,16 +10,29 @@ function calculateMargin(buy_price, sell_price)
     return margin;
 }
 
+function parceLetter(num){
+    multiplier = num.substr(-1).toLowerCase();
+    if (multiplier == "k")
+      return parseFloat(num) * 1000;
+    else if (multiplier == "m")
+      return parseFloat(num) * 1000000;
+    else if (multiplier == "b")
+      return parseFloat(num) * 1000000000;
+    else 
+      return parseFloat(num);
+}
+
 function calculate()
 {
-    buy_price = document.getElementById("bprice").value;
-    sell_price = document.getElementById("sprice").value;
-    volume = document.getElementById("volume").value;
+    buy_price = parceLetter(document.getElementById("bprice").value);
+    sell_price = parceLetter(document.getElementById("sprice").value);
+    volume = parceLetter(document.getElementById("volume").value);
 
     if(buy_price !== "" && sell_price !== ""){
         tax = calculateTax(sell_price);
         margin = calculateMargin(buy_price, sell_price);
         total = margin * volume;
+        roi = Math.round((margin / buy_price * 100) / 100) + "%";
 
         tax_str = tax.toLocaleString("en-US");
         margin_str = margin.toLocaleString("en-US");
@@ -28,5 +41,6 @@ function calculate()
         document.getElementById("tax_output").innerHTML = tax_str;
         document.getElementById("margin_output").innerHTML = margin_str;
         document.getElementById("total_output").innerHTML = total_str;
+        document.getElementById("roi_output").innerHTML = roi;
     }
 }
